02fbcb02d445df06de50e371fddcb16b495257e9
Rithvik <rithvikreddybasani@gmail.com>
2024-03-15 18:43:50 +0530

    added stopwatch

--- one/src/App.js
+++ one/src/App.js
@@ -1,25 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 
 const App = () => {
-  const [square, setSquares] = useState(Array(9).fill(""));
-  const [user, setUser] = useState("X");
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-  const check = (index) => {
-    const fun = [...square];
-    fun[index] = user;
-    setSquares(fun);
-    setUser(user === "X" ? "0" : "X");
+  useEffect(() => {
+    let intervalid;
+    if (isRunning) {
+      intervalid = setInterval(() => {
+        let some = time + 1;
+        setTime(some);
+      }, 100);
+    } else {
+      clearInterval(intervalid);
+    }
+    return () => clearInterval(intervalid);
+  }, [time, isRunning]);
+
+  const start = () => {
+    setIsRunning(true);
+  };
+
+  function padStart(value) {
+    return String(value).padStart(2, "0");
+  }
+
+  const end = () => {
+    setIsRunning(false);
+  };
+
+  const reset = () => {
+    setIsRunning(false);
+    setTime(0);
   };
 
   return (
     <>
-      <div className="container">
-        {square.map((ind, index) => (
-          <Header ind={ind} index={index} select={check} />
-        ))}
-      </div>
+      <h1>StopWatch</h1>
+      <p>
+        {padStart(Math.floor(time / 60))}:{padStart(time % 60)}
+      </p>
+      <button onClick={start}>Start</button>
+      <button onClick={end}>Stop</button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 };
