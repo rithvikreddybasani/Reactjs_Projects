c09bb100340d150ed7a6ab8f791601bc618662a8
Rithvik <rithvikreddybasani@gmail.com>
2024-06-28 22:42:18 +0530

    updated index.html

--- react-portfolio/src/data/constants.js
+++ react-portfolio/src/data/constants.js
@@ -1,6 +1,10 @@
 export const Bio = {
   name: "Rithvik Reddy basani",
-  roles: ["Full Stack Developer", "Problem Solver", "AI and Machine Learning"],
+  roles: [
+    "Full Stack Developer",
+    "Problem Solver",
+    "Machine Learning Enthusiast",
+  ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/rithvikreddybasani",
